b481a13d8227a834b5ed1e977fa728a0e10db8e9
jnckim2003 <jnckim2003@gmail.com>
2016-05-04 02:52:55 -0700

    Update MancalaComponent.java

--- MancalaComponent.java
+++ MancalaComponent.java
@@ -1,5 +1,49 @@
 package mancala;
 
-public class MancalaComponent {
-    
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MancalaComponent extends JFrame
+{
+	private static final int WIDTH = 750;
+	private static final int HEIGHT = 450;
+	private MancalaBoard board;
+	
+	public MancalaComponent(DesignLayout[] designs)
+	{
+		setSize(WIDTH, HEIGHT);
+		displayBoard(designs[0]);
+		setVisible(true);
+		
+		MancalaDialog selectScreen = new MancalaDialog(this, designs);
+		selectScreen.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		selectScreen.display();
+		this.
+		start(selectScreen.getStoneNum(), selectScreen.getDesign());
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setResizable(false);
+	}
+	
+	public void start(int stones, DesignLayout design)
+	{
+		System.out.println("stone number = " + stones + ", design number = " + design.getName());
+		
+		displayBoard(design);
+	}
+
+	public void displayBoard(DesignLayout design)
+	{
+		//removeAll();
+		board = new MancalaBoard(design);
+		add(board);
+	}
+	
+	
 }
