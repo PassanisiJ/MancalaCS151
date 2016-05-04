b66df3adc5833aef174f12dac65798df9cbb3657
jnckim2003 <jnckim2003@gmail.com>
2016-05-04 03:05:30 -0700

    Update DesignOne.java

--- DesignOne.java
+++ DesignOne.java
@@ -1,5 +1,32 @@
 package mancala;
 
-public class DesignOne {
-    
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
+
+public class DesignOne extends DesignLayout
+{
+
+	public DesignOne(int nPlayers, int boardLength)
+	{
+		super(nPlayers, boardLength);
+		System.out.println("one");
+	}
+
+	@Override
+	public void redraw(Graphics g)
+	{
+		System.out.println("Design One!");
+		Graphics2D graphics2g = (Graphics2D) g;
+	    RoundRectangle2D roundedRectangle = new RoundRectangle2D.Double(150, 160, 300, 200, 30, 30);
+	    graphics2g.draw(roundedRectangle);
+		
+	}
+
+	@Override
+	public String getName()
+	{
+		return "Design One";
+	}
+
 }
