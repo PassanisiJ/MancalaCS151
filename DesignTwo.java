a6ba909a53bfdec17b16912d7b8d6fe7e879f151
jnckim2003 <jnckim2003@gmail.com>
2016-05-04 03:06:24 -0700

    Update DesignTwo.java

--- DesignTwo.java
+++ DesignTwo.java
@@ -1,5 +1,25 @@
 package mancala;
 
-public class DesignTwo {
-    
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
+
+
+public class DesignTwo extends DesignLayout
+{
+	@Override
+	public void redraw(Graphics g)
+	{
+		System.out.println("Design Two!");
+		Graphics2D graphics2g = (Graphics2D) g;
+	    RoundRectangle2D roundedRectangle = new RoundRectangle2D.Double(50, 60, 300, 200, 30, 30);
+	    graphics2g.draw(roundedRectangle);
+		
+	}
+
+	@Override
+	public String getName()
+	{
+		return "Design Two";
+	}
 }
