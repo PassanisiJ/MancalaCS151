9ed0835cdea636ebb031d3a1289c96c0cc381676
jnckim2003 <jnckim2003@gmail.com>
2016-05-04 03:00:47 -0700

    Update MancalaBoard.java

--- MancalaBoard.java
+++ MancalaBoard.java
@@ -1,5 +1,21 @@
 package mancala;
 
-public class MancalaBoard {
-    
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+public class MancalaBoard extends JComponent
+{
+	private DesignLayout layout;
+	
+	public MancalaBoard(DesignLayout layout)
+	{
+		this.layout = layout;
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		//System.out.println("design # = " + layout.getName());
+		layout.redraw(g);
+	}
 }
