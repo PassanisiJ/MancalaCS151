65afcf64c70b459c5df3b22359e281e53c035e27
PassanisiJ <passanisijd@yahoo.com>
2016-05-06 16:19:34 -0700

    Delete EmptyPitException.java

--- EmptyPitException.java
+++
@@ -1,8 +0,0 @@
-package mancala;
-
-@SuppressWarnings("serial")
-public class EmptyPitException extends Exception
-{
-    public EmptyPitException() {}
-    public EmptyPitException(String reason) {super(reason);}
-}
